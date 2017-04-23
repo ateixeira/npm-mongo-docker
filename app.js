aba48128002370c07d102d0fe4ec291cbcd7c133
Andre Teixeira <andre.teixeira@gmail.com>
2017-04-23 02:57:07 -0300

    Configuring mongo/mongoose on the example app

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 var express = require('express');
 var app = express();
+var mongoose = require(‘mongoose’);
+
+// MongoDB setup
+mongoose.connect(“mongodb://mongo:27017”);
+
+//routes
 app.get('/', function(req, res){
   res.send("Hello World");
 });
+
+//Server startup
 app.listen(3000, function(){
   console.log('Example app listening on port 3000!');
 });
