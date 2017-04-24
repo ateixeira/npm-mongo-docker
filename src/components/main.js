447476faef79941a34019cea28b04da1813eeacc
Andre Teixeira <andre.teixeira@gmail.com>
2017-04-24 16:46:50 -0300

    Creating stub Main component

---
+++ src/components/main.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Main extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="main">
+                Main component
+            </div>
+        );
+    }
+
+}
+
+export default Main;
---
+++ src/containers/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return(
+            <div> App Container </div>
+        )
+    }
+}
