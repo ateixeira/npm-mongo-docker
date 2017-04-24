0808b2fbfa6563b5263d4f1aef9500aadb0fef65
Andre Teixeira <andre.teixeira@gmail.com>
2017-04-24 17:33:36 -0300

    Intercepting all requests on server side, trying to match them with the routes file and if so rendering the component from server side

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ import mongoose from 'mongoose';
 import { Server } from 'http';
 
 import { Provider } from 'react-redux';
+import { match, RouterContext } from 'react-router';
+import { renderToString } from 'react-dom/server';
+
+import routes from './src/routes';
+import NotFoundPage from './src/components/notfoundpage';
+import configureStore from './src/configureStore'
+
 
 const app = express();
 const server = new Server(app);
@@ -30,9 +37,45 @@ if (process.env.NODE_ENV !== 'production') {
 mongoose.connect("mongodb://mongo:27017");
 
 
-//routes
-app.get('/', function(req, res){
-  res.send("Hello World");
+// Server side rendering shared components and shared routes
+app.get('*', (req, res) => {
+    match(
+        { routes, location: req.url },
+        (err, redirectLocation, renderProps) => {
+
+            // in case of error display the error message
+            if (err) {
+                return res.status(500).send(err.message);
+            }
+
+            // in case of redirect propagate the redirect to the browser
+            if (redirectLocation) {
+                return res.redirect(302, redirectLocation.pathname + redirectLocation.search);
+            }
+
+            // Create a new Redux store instance
+            const store = configureStore();
+
+            // generate the React markup for the current route
+            let markup;
+            if (renderProps) {
+                // if the current route matched we have renderProps
+                markup = renderToString(
+                    <Provider store={store}>
+                        <RouterContext {...renderProps}/>
+                    </Provider>
+                );
+
+            } else {
+                // otherwise we can render a 404 page
+                markup = renderToString(<NotFoundPage/>);
+                res.status(404);
+            }
+
+            // render the index template with the embedded React markup
+            return res.render('index', { markup });
+        }
+    );
 });
 
 
