349c4f5017b1fb385ab42c7851ab11f8f09f1b11
Andre Teixeira <andre.teixeira@gmail.com>
2017-04-24 17:32:38 -0300

    Adding the rootReducer to the store config function

--- src/configureStore.js
+++ src/configureStore.js
@@ -3,11 +3,13 @@ import thunkMiddleware from 'redux-thunk';
 import { createLogger } from 'redux-logger';
 import { routerMiddleware } from 'react-router-redux'
 import { browserHistory } from 'react-router';
+import rootReducer from './reducers/index';
 
 const loggerMiddleware = createLogger()
 
 export default function configureStore(preloadedState) {
   return createStore(
+  	rootReducer,
     preloadedState,
     applyMiddleware(
       thunkMiddleware,
