fb4aa96f71374947fe0c74d3019ff8091e63b0f2
Andre Teixeira <andre.teixeira@gmail.com>
2017-04-24 18:48:56 -0300

    Changing notfoundpage component from React.createClass to extends React.Component

--- src/components/notfoundpage.js
+++ src/components/notfoundpage.js
@@ -1,16 +1,19 @@
 import React from 'react';
- 
-const NotFoundPage = React.createClass({
 
-    // RENDER
-    render: function() {
-        
+class NotFoundPage extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
         return (
             <div className="notfound">
                 404
             </div>
         );
     }
-});
+
+}
 
 export default NotFoundPage;
