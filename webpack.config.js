5c5790f9fc62e2e6dad3ba0a0220cb2b8eae9a62
Andre Teixeira <andre.teixeira@gmail.com>
2017-04-24 16:22:25 -0300

    Changing from webpack-dev-server to webpack-dev-middleware + webpack-hot-middleware configuration… Not so automatic, but good to learn about

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,30 @@
-const OpenBrowserPlugin = require('open-browser-webpack-plugin');
 const path = require('path')
 const webpack = require('webpack');
+const OpenBrowserPlugin = require('open-browser-webpack-plugin');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const version = require('./package.json').version;
 
+
+// Source map
+const devtool = 'eval';
+
+
 // App entry
-const entry =  __dirname + '/src/index.js';
+const entry =  [
+    'webpack-hot-middleware/client',
+    './src/index.js'
+]
 
 
 // App js output
 const output =  {
-  filename: 'page/[name]/index.js',
-  chunkFilename: 'chunk/[name].[chunkhash:5].chunk.js',
-};
+    path: path.join(__dirname, 'public'),
+    filename: "bundle.js",
+    publicPath: '/public/'
+}
 
 
-// Source map
-const devtool = 'source-map';
-
-
-// Loader
+// Loaders
 const loaders = [
     {
         loader: "babel-loader",
@@ -44,7 +49,7 @@ const loaders = [
 
 
 // Dev plugin
-var devPlugins =  [
+const devPlugins =  [
     // Hot module
     new webpack.HotModuleReplacementPlugin(),
     // Avoid stopping the app execution on error
@@ -55,33 +60,25 @@ var devPlugins =  [
     }),
     // CSS Package
     new ExtractTextPlugin({
-        filename: 'css.css',
+        filename: 'style.css',
         allChunks: true
     }),
 ]
 
 
-// dev server
-var devServer = {
-    contentBase: './server',
-    historyApiFallback: false,
-    port: 3000, 
-    hot: true, // Hot Module Replacement
-    inline: true, // Livereload
-    host: '0.0.0.0',
-};
+// SASS Loader
+const sassLoader = {
+    includePaths: [path.resolve(__dirname, "./src/styles")]
+}
 
 
 module.exports = {
     devtool: devtool,
     entry: entry,
-    output: {
-        path: __dirname + '/build',
-        filename: 'bundle.js',
-    },
+    output: output,
     module: {
         loaders: loaders
     },
     plugins: devPlugins,
-    devServer: devServer
+    sassLoader: sassLoader
 };
