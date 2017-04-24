181eb38af9c3f2cfe5c03ebd60aa6bba6f6dfa2f
Andre Teixeira <andre.teixeira@gmail.com>
2017-04-24 17:19:50 -0300

    Creating a notfoundpage component

---
+++ src/components/notfoundpage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+ 
+const NotFoundPage = React.createClass({
+
+    // RENDER
+    render: function() {
+        
+        return (
+            <div className="notfound">
+                404
+            </div>
+        );
+    }
+});
+
+export default NotFoundPage;
