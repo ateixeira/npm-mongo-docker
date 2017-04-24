0a811b14554113587955112ca5936cb32543716a
Andre Teixeira <andre.teixeira@gmail.com>
2017-04-24 16:34:57 -0300

    Setting up dev server on express entry file in case NODE ENV var is not set to production

--- app.js
+++ app.js
@@ -2,13 +2,32 @@ var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
 
+
+// Development server configuration
+if (process.env.NODE_ENV !== 'production') {
+    const webpack = require('webpack')
+    const webpackDevMiddleware = require('webpack-dev-middleware')
+    const webpackHotMiddleware = require('webpack-hot-middleware')
+    const config = require('./webpack.dev.config.js')
+    const compiler = webpack(config)
+
+    app.use(webpackHotMiddleware(compiler))
+    app.use(webpackDevMiddleware(compiler, {
+        noInfo: true,
+        publicPath: config.output.publicPath
+    }))
+}
+
+
 // MongoDB setup
 mongoose.connect("mongodb://mongo:27017");
+
 
 //routes
 app.get('/', function(req, res){
   res.send("Hello World");
 });
+
 
 //Server startup
 app.listen(3000, function(){
