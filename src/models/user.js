ebf443c9a1040b380b728dedc006ce5e4e0778ea
Andre Teixeira <andre.teixeira@gmail.com>
2017-04-25 00:16:36 -0300

    Creating base user model schema based on mongoldb official docs. https://www.mongodb.com/blog/post/password-authentication-with-mongoose-part-1

---
+++ src/models/user.js
@@ -0,0 +1,56 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+
+const Schema = mongoose.Schema;
+const SALT_WORK_FACTOR = 10;
+
+//===================================================================================================
+// User Schema -- From https://www.mongodb.com/blog/post/password-authentication-with-mongoose-part-1
+//===================================================================================================
+
+const UserSchema = new Schema({
+    username: { type: String, required: true, index: { unique: true } },
+    password: { type: String, required: true }
+});
+
+
+//================================
+// User Schema ODM methods
+//================================
+
+// Pre-saves user to database, hash of the password if it is new or was modified
+
+UserSchema.pre(save, function(next) {
+    var user = this;
+
+    // only hash the password if it has been modified (or is new)
+    if (!user.isModified('password')) return next();
+
+    // generate a salt
+    bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+        if (err) return next(err);
+
+        // hash the password using our new salt
+        bcrypt.hash(user.password, salt, function(err, hash) {
+            if (err) return next(err);
+
+            // override the cleartext password with the hashed one
+            user.password = hash;
+            next();
+        });
+    });
+
+
+});
+
+
+// Compares passwords for login
+
+UserSchema.methods.comparePassword = function(candidatePassword, cb) {
+    bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+        if (err) return cb(err);
+        cb(null, isMatch);
+    });
+};
+
+module.exports = mongoose.model(User, UserSchema);
