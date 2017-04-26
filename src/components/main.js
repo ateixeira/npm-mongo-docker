746f4667b029ca732335baa738706e33cccf7e32
Andre Teixeira <andre.teixeira@gmail.com>
2017-04-26 16:35:25 -0300

    Creating basic navbar layout on main component using bootstrap 4

--- src/components/main.js
+++ src/components/main.js
@@ -8,8 +8,24 @@ class Main extends React.Component {
 
     render() {
         return (
-            <div className="main">
-                Main component
+            <div className="container">
+                <nav className="navbar navbar-toggleable-md fixed-top navbar-inverse bg-inverse">
+                    <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <a className="navbar-brand" href="#">Navbar</a>
+
+
+                    <div className="collapse navbar-collapse" id="navbarsExampleDefault">
+                        <ul className="navbar-nav mr-auto">
+                            <li className="nav-item active">
+                                <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                            </li>
+                        </ul>
+                    </div>
+
+
+                </nav>
             </div>
         );
     }
--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { render } from 'react-dom';
 import Root from './containers/root';
 
-import './styles/bootstrap.scss'
+import './styles/style.scss'
 
 render(
     <Root />
-    , document.getElementById('container')
+    , document.getElementById('root')
 );
