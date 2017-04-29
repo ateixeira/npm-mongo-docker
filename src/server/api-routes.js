5c304dc637a60a990b7d5aab3d3bd7b8e68c5b79
Andre Teixeira <andre.teixeira@gmail.com>
2017-04-29 11:07:32 -0300

    Building API findById for user model

--- src/server/api-routes.js
+++ src/server/api-routes.js
@@ -1,23 +1,43 @@
 import User from '../models/user';
+import ObjectID from 'mongodb';
+
+var USERS_COLLECTION = "users";
 
 module.exports = function(app){
 
-	/*  "/api/users"
-	 *    GET: finds all users
-	 *    POST: creates a new user
-	 */
+    /*  "/api/users"
+     *    GET: finds all users
+     *    POST: creates a new user
+     */
 
-	app.get('/api/users', function(req, res) {
-		User.find(function (err, users) {
-			if (err) return next(err);
-			res.json(users);
-		});
-	});
+    app.get('/api/users', function(req, res) {
+        User.find(function (err, users) {
+            if (err) return next(err);
+            res.status(200).json(users);
+        });
+    });
 
-	app.post('/api/users', function(req, res) {
-		res.json({
-			"message": "POST de um novo usuário!!"
-		})
-	});
+    app.post('/api/users', function(req, res) {
+        res.json({
+            "message": "POST de um novo usuário!!"
+        })
+    });
+
+
+    /*  "/api/users/:id"
+     *    GET: find user by id
+     *    PUT: update user by id
+     *    DELETE: deletes user by id
+     */
+
+    app.get('/api/users/:id', function(req, res) {
+        User.findById( req.params.id, function(err, doc) {
+            if (err) {
+                res.status(500).json(err.message);
+            } else {
+                res.status(200).json(doc);
+            }
+        });
+    });
 
 }
