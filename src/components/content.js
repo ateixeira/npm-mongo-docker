8864ad3a2013fa0e05582e19455121ec4fb0e228
Andre Teixeira <andre.teixeira@gmail.com>
2017-05-03 00:20:57 -0300

    Modeling modal + user registration form first html on user component

--- src/components/content.js
+++ src/components/content.js
@@ -47,6 +47,51 @@ class Content extends React.Component {
                                 </tbody>
                             </table>
                         </div>
+
+                        <button type="button" className="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+                            Create User
+                        </button>
+
+                        <div className="modal fade" id="exampleModal" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div className="modal-dialog" role="document">
+                                <div className="modal-content">
+                                    <div className="modal-header">
+                                        <h5 className="modal-title" id="exampleModalLabel">User</h5>
+                                        <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                                          <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+                                    <div className="modal-body">
+                                        <div className="container">
+                                            <div className="col-lg-12 well">
+                                                <div className="row">
+                                                    <div className="col-sm-6 form-group">
+                                                        <label>First Name</label>
+                                                        <input type="text" placeholder="Enter First Name Here.." className="form-control" />
+                                                    </div>
+                                                    <div className="col-sm-6 form-group">
+                                                        <label>Last Name</label>
+                                                        <input type="text" placeholder="Enter Last Name Here.." className="form-control" />
+                                                    </div>
+                                                </div>
+                                                <div className="form-group">
+                                                    <label>Email Address</label>
+                                                    <input type="text" placeholder="Enter Email Address Here.." className="form-control" />
+                                                </div>  
+                                                <div className="form-group">
+                                                    <label>Website</label>
+                                                    <input type="text" placeholder="Enter Website Name Here.." className="form-control" />
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="modal-footer">
+                                        <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+                                        <button type="button" className="btn btn-primary">Save changes</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </main>
                 </div>
             </div>
