7867a0bd8eefc9ff16875313db3b862bcc01d5ae
Andre Teixeira <andre.teixeira@gmail.com>
2017-05-04 19:48:41 -0300

    Adding name params to user modal

--- src/models/user.js
+++ src/models/user.js
@@ -10,6 +10,7 @@ const SALT_WORK_FACTOR = 10;
 
 const UserSchema = new Schema({
     email: { type: String, required: true, index: { unique: true } },
+    name: { type: String, required: true },
     password: { type: String, required: true }
 });
 
