8781244834325e7234ee4a991b6223ba697c69aa
Andre Teixeira <andre.teixeira@gmail.com>
2017-05-03 22:24:17 -0300

    Indentation on redux actions sync test cases

--- tests/actions.test.js
+++ tests/actions.test.js
@@ -10,26 +10,29 @@ const mockStore = configureMockStore(middlewares)
 
 
 describe('sync actions', () => {
-  it('shouldFetchUsers returns true when users are not yet loaded on usersList', () => {
-    const state = {
-      usersList: {
-      },
-    };
+    it('shouldFetchUsers returns true when users are not yet loaded on usersList', () => {
+        const state = {
+            usersList: {
+            },
+        };
 
-    expect(shouldFetchUsers(state)).toEqual(true);
-  });
+        expect(shouldFetchUsers(state)).toEqual(true);
+    });
 
 
   it('shouldFetchUsers returns false when isFetching is true', () => {
     const state = {
-      usersList: {
-        users: {
-          isFetching: true,
+        usersList: {
+            users: {
+                isFetching: true,
+            },
         },
-      },
     };
 
     expect(shouldFetchUsers(state)).toBe(false);
   });
+
+});
+
 
 });
